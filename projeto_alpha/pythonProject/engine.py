99d367461de185d75f9477638c897f8a20b31d43
Dharuf <150285896+Dharuf@users.noreply.github.com>
2023-12-14 12:54:14 -0300

    Add files via upload

---
+++ projeto_alpha/pythonProject/engine.py
@@ -0,0 +1,31 @@
+import cv2
+import face_recognition as fr
+
+def reconhece_face(url_foto):
+    foto = fr.load_image_file(url_foto)
+    rostos = fr.face_encodings(foto)
+    if(len(rostos) > 0):
+        return True, rostos
+
+    return False, []
+
+def get_rostos():
+    rostos_conhecidos = []
+    nomes_dos_rostos  = []
+
+    wendel1 = reconhece_face("./Pessoas/wendel.png")
+    if(wendel1[0]):
+        rostos_conhecidos.append(wendel1[1][0])
+        nomes_dos_rostos.append("Wendel")
+
+    Diogo1 = reconhece_face("./Pessoas/diogo.png")
+    if(Diogo1[0]):
+        rostos_conhecidos.append(Diogo1[1][0])
+        nomes_dos_rostos.append("Diogo")
+
+    evaldo1 = reconhece_face("./Pessoas/evaldo.png")
+    if (evaldo1[0]):
+        rostos_conhecidos.append(evaldo1[1][0])
+        nomes_dos_rostos.append("Evaldo")
+
+    return rostos_conhecidos, nomes_dos_rostos
